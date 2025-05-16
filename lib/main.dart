59a9b26d82703d506b09399dda4fc7722aeeae5b
reisdomenejoaohigor <reisdomenejoaohigor@gmail.com>
2025-05-16 00:34:31 +0000

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'App Planetas'),
+      home: const MyHomePage(title: 'App Alerta Evasão'),
     );
   }
 }
